d9d818675e2d468e2ad5ba76bc192c2697ce5885
gitadr <mail@aaronroot.net>
2018-09-04 15:09:03 +0100

    remove react folder

--- react/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Home from "./components/Home";
-import "./css/style.css";
-
-ReactDom.render(<Home />, document.getElementById("root"));
